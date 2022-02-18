4687fc1cee22e53fbd7e6c89ca50d86f5d5d2417
Tokuda Frog <53279919+chuhakhanh@users.noreply.github.com>
2022-02-19 01:19:08 +0700

    edit new file

--- configure.sh
+++ configure.sh
@@ -18,9 +18,21 @@ ansible-playbook snapshot_vms_k8s_cluster_c4.yml
 
 kubeadm init \
   --apiserver-advertise-address=10.1.17.81 \
-  --pod-network-cidr 172.16.0.0/16 \
+  --pod-network-cidr 192.168.128.0/24 \
   --upload-certs
 
+#https://projectcalico.docs.tigera.io/getting-started/kubernetes/quickstart
+#kubectl create -f https://projectcalico.docs.tigera.io/manifests/tigera-operator.yaml
+#kubectl create -f https://projectcalico.docs.tigera.io/manifests/custom-resources.yaml
+
+kubectl apply -f https://docs.projectcalico.org/v3.10/manifests/calico.yaml
+kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml 
+kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yaml
+
+kubectl describe node c4-m-1
+kubeadm reset
+rm -f /etc/cni/net.d/*
+rm -rf $HOME/.kube
 
 # C2:
  
